b79a6d8a241983b53881871bf95fe4b6c0a391ac
2510subham <subhamagarwal400@gmail.com>
2023-09-30 14:59:07 +0530

    checking

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+);
+app.get("/post", (req, res) => {
+    res.status(200).json({ title: "Post 1", content: "This is my first post" })
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
