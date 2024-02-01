febbca008c39ba787f8c1c39501a7d9ed7e4d0f5
2510subham <subhamagarwal400@gmail.com>
2024-02-01 17:25:55 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 }
 );
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
 app.get("/post", (req, res) => {
     res.status(200).json({ title: "Post 1", content: "This is my first post" })
 });
